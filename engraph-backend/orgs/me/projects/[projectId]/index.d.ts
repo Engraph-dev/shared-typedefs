e34a138fbd2aad089a9177148832c24c14981711
Arnav Deo <arnav.nitin.deo@gmail.com>
2025-01-10 07:15:28 +0530

    feat: 🎸 Update types to include projectBranch attribute

--- engraph-backend/orgs/me/projects/[projectId]/index.d.ts
+++ engraph-backend/orgs/me/projects/[projectId]/index.d.ts
@@ -3,7 +3,10 @@ import type { MakeOptional } from "../../../../common"
 
 export type UpdateProjectParams = ProjectId
 export type UpdateProjectBody = MakeOptional<
-	Omit<CreateProjectBody, "projectIdentifier" | "projectSourceType">
+	Omit<
+		CreateProjectBody,
+		"projectIdentifier" | "projectSourceType" | "projectBranch"
+	>
 >
 
 export type DeleteProjectParams = ProjectId
