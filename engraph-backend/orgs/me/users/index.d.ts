957983a9c2708e80b0b8bbeca43d603e162300a2
Arnav Deo <arnav.nitin.deo@gmail.com>
2025-01-21 12:41:00 +0530

    feat: 🎸 Add search users endpoint type definitions

--- engraph-backend/orgs/me/users/index.d.ts
+++ engraph-backend/orgs/me/users/index.d.ts
@@ -1,4 +1,8 @@
-import type { PagedQuery } from "../../../common"
+import type {
+	ExtendPagedQuery,
+	MakeOptional,
+	PagedQuery,
+} from "../../../common"
 import type { MiniUser } from "../../../common/users"
 import type { User } from "@prisma/client"
 
@@ -9,5 +13,15 @@ export type CreateUserResponse = { userData: MiniUser }
 export type GetUsersQuery = PagedQuery
 
 export type GetUsersResponse = {
+	orgUsers: MiniUser[]
+}
+
+export type SearchUsersQuery = ExtendPagedQuery<
+	MakeOptional<{
+		searchQuery: string
+	}>
+>
+
+export type SearchUsersResponse = {
 	orgUsers: MiniUser[]
 }
