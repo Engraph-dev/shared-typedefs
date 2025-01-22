03d9714fd0bdbd6d5a9698a29dd0065de9e662bf
Arnav Deo <arnav.nitin.deo@gmail.com>
2025-01-22 13:05:29 +0530

    feat: 🎸 Add workflow response type definitions

--- engraph-backend/orgs/me/projects/[projectId]/workflows/[workflowId]/index.d.ts
+++ engraph-backend/orgs/me/projects/[projectId]/workflows/[workflowId]/index.d.ts
@@ -2,10 +2,26 @@ import type { ProjectIdWorkflowId } from ".."
 
 export type QueryWorkflowParams = ProjectIdWorkflowId
 
+export type GetWorkflowParams = ProjectIdWorkflowId
+
+export type GetWorkflowResponse = {
+	workflowData: {
+		moduleNodes: any[]
+		symbolNodes: any[]
+		externalModuleNodes: any[]
+		externalSymbolNodes: any[]
+		nodeLinks: any[]
+	}
+}
+
 export type QueryWorkflowBody = {
 	userQuery: string
 }
 
 export type QueryWorkflowResponse = {
-	queryResponse: string
+	queryData: {
+		execQuery: string
+		execContext: string
+		chatResponse: string
+	}
 }
