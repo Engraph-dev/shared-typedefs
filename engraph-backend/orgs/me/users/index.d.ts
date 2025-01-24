bfbddbaaf77f5cb21e63331c5d30bbbe46ec93ba
Arnav Deo <arnav.nitin.deo@gmail.com>
2025-01-24 19:45:32 +0530

    feat: 🎸 Add get/update organization user endpoints

--- engraph-backend/orgs/me/users/index.d.ts
+++ engraph-backend/orgs/me/users/index.d.ts
@@ -1,14 +1,13 @@
-import type {
-	ExtendPagedQuery,
-	MakeOptional,
-	PagedQuery,
-} from "../../../common"
-import type { MiniUser } from "../../../common/users"
-import type { User } from "@prisma/client"
+import type { User } from "@prisma/client";
+import type { ExtendPagedQuery, MakeOptional, PagedQuery } from "../../../common";
+import type { MiniUser } from "../../../common/users";
 
 export type CreateUserBody = Omit<User, "userOrgId" | "userId" | "userVerified">
 
-export type CreateUserResponse = { userData: MiniUser }
+export type UserResponse = { userData: MiniUser }
+
+export type CreateUserResponse = UserResponse
+export type CreateUserResponse = UserResponse
 
 export type GetUsersQuery = PagedQuery
 
